d351cae535b982cc816f296b026dc34a7ee436b8
GG <sudodebug@gmail.com>
2023-03-03 14:09:54 +0500

    Added DayWeather, delete Game, Team

--- tournament/models.py
+++ tournament/models.py
@@ -1,14 +1,8 @@
 from django.db import models
 
 
-class Team(models.Model):
-    name = models.CharField(max_length=64)
-
-
-class Game(models.Model):
-    home_team = models.ForeignKey(Team, related_name='game_at_home', on_delete=models.CASCADE)
-    home_team_points = models.IntegerField()
-    rival_team = models.ForeignKey(Team, related_name='rival_game', on_delete=models.CASCADE)
-    rival_team_points = models.IntegerField()
+class DayWeateher(models.Model):
     date = models.DateField()
-
+    precipitation = models.FloatField()
+    temperature = models.FloatField()
+    was_raining = models.BooleanField()
