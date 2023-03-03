b2d46c897caab519b2ccd2274b73c93134353fb1
GG <sudodebug@gmail.com>
2023-03-03 14:17:49 +0500

    Added book

--- tournament/models.py
+++ tournament/models.py
@@ -12,3 +12,12 @@ class House(models.Model):
     apartments = models.IntegerField()
     house_number = models.IntegerField()
     street = models.CharField(max_length=64)
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=30)
+    author = models.CharField(max_length=30)
+    pages_count = models.IntegerField()
+    genre = models.CharField(max_length=30)
+    book_cost = models.DecimalField(max_digits=6, decimal_places=2)
+    status = models.CharField(max_length=30)
