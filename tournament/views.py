b180be7cc180580ca6f29975186464249eacd4e9
GG <sudodebug@gmail.com>
2023-03-05 13:34:50 +0500

    Conf index

--- tournament/views.py
+++ tournament/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Song
 
 
 def main(request):
-    return render(request, 'index.html', {})
+    song = Song.objects.all()
+    return render(request, 'index.html', {'song': song})
