df15e77a8ffc60a1b888343aaff98c0020082900
GG <sudodebug@gmail.com>
2023-03-05 13:05:13 +0500

    Del son, create and migrate Song

--- tournament/models.py
+++ tournament/models.py
@@ -2,9 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Son(models.Model):
-    class Song(models.Model):
-        title = models.CharField(max_length=128)
-        artist = models.CharField(max_length=128)
-        path_to_file = models.FileField(upload_to='static/')
-        favorite_by = models.ManyToManyField(User, related_name='favourite_songs')
+class Song(models.Model):
+    title = models.CharField(max_length=128)
+    artist = models.CharField(max_length=128)
+    path_to_file = models.FileField(upload_to='static/')
+    favorite_by = models.ManyToManyField(User, related_name='favourite_songs')
