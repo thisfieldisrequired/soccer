08b0d46274319aa448bec949ef492be6b4c67b02
GG <sudodebug@gmail.com>
2023-03-06 13:22:33 +0500

    Create Plane, Airport, Flight

--- tournament/models.py
+++ tournament/models.py
@@ -1,17 +1,23 @@
 from django.db import models
 
 
-class User(models.Model):
-    id = models.IntegerField(primary_key=True)
-    name = models.TextField()
-
-class MobilePhone(models.Model):
-    id = models.IntegerField(primary_key=True)
-    phone = models.CharField(max_length=10)
-    owner = models.OneToOneField(User, on_delete=models.CASCADE, related_name='myphone')
+class Plane(models.Model):
+    manufacturer = models.CharField(max_length=120)  # Производитель
+    model = models.CharField(max_length=120)  # Модель
+    seats = models.IntegerField()  # Количество мест
+    runway_min_lenght = models.IntegerField()  # Минимальная длина полосы
 
 
-class Chat(models.Model):
-    id = models.IntegerField(primary_key=True)
-    title = models.CharField(max_length=50)
-    users = models.ManyToManyField(User, related_name='chats')
+class Airport(models.Model):
+    code = models.CharField(max_length=120)  # Код
+    country = models.CharField(max_length=120)  # Страна расположения
+    city = models.CharField(max_length=120)  # Город расположения
+    runway_length = models.IntegerField()  # Длина полосы (м)
+
+
+class Flight(models.Model):
+    number = models.CharField(max_length=120)  # Номер рейса
+    plane = models.ForeignKey(Plane, on_delete=models.CASCADE, related_name='plane')  # Используемый самолет
+    from_airport = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name='from_airport')  # Аэропорт вылета
+    to_airport = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name='to_airport')  # Аэропорт назначения
+    passengers_count = models.IntegerField()  # Пассажиров зарегистрировано
