db1d40087052522b2997cec9bb63bcff974f92da
GG <sudodebug@gmail.com>
2023-03-06 12:32:05 +0500

    Create Chat

--- tournament/models.py
+++ tournament/models.py
@@ -9,3 +9,9 @@ class MobilePhone(models.Model):
     id = models.IntegerField(primary_key=True)
     phone = models.CharField(max_length=10)
     owner = models.OneToOneField(User, on_delete=models.CASCADE, related_name='myphone')
+
+
+class Chat(models.Model):
+    id = models.IntegerField(primary_key=True)
+    title = models.CharField(max_length=50)
+    users = models.ManyToManyField(User, related_name='chats')
